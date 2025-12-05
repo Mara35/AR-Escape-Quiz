dbe13352f21031dffcffe303951aaaf5e980f25b
Mara35 <sm6981@mci4me.at>
2025-12-05 12:21:34 +0100

    Door Open/Close

---
+++ Escape Quiz/Assets/Scripts/Door.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace DoorScript
+{
+	[RequireComponent(typeof(AudioSource))]
+
+
+public class Door : MonoBehaviour {
+	public bool open;
+	public float smooth = 1.0f;
+	float DoorOpenAngle = -90.0f;
+    float DoorCloseAngle = 0.0f;
+	public AudioSource asource;
+	public AudioClip openDoor,closeDoor;
+	// Use this for initialization
+	void Start () {
+		asource = GetComponent<AudioSource> ();
+	}
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Touch-Eingabe prüfen
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                Touch t = Input.GetTouch(0);
+                Ray ray = Camera.main.ScreenPointToRay(t.position);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.transform == transform)
+                    {
+                        OpenDoor();
+                    }
+                }
+            }
+
+            // --- dein bestehender Öffnungs-/Schließ-Code ---
+            if (open)
+            {
+                var target = Quaternion.Euler(0, -90, 0);
+                transform.localRotation = Quaternion.Slerp(transform.localRotation, target, Time.deltaTime * 5 * smooth);
+            }
+            else
+            {
+                var target1 = Quaternion.Euler(0, 0, 0);
+                transform.localRotation = Quaternion.Slerp(transform.localRotation, target1, Time.deltaTime * 5 * smooth);
+            }
+        }
+
+        public void OpenDoor(){
+		open =!open;
+		asource.clip = open?openDoor:closeDoor;
+		asource.Play ();
+	}
+}
+}
