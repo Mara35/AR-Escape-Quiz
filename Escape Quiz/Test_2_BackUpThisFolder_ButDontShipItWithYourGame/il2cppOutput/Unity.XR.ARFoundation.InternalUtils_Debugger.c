1baa609c0c07185f0b98e5db124bc4a0049e5e0b
Mara35 <sm6981@mci4me.at>
2026-01-21 14:17:25 +0100

    changed Reference Image

---
+++ Escape Quiz/Test_2_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Unity.XR.ARFoundation.InternalUtils_Debugger.c
@@ -0,0 +1,237 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+#if IL2CPP_MONO_DEBUGGER
+static const Il2CppMethodExecutionContextInfo g_methodExecutionContextInfos[6] = 
+{
+	{ 28659, 0,  3 },
+	{ 28659, 1,  3 },
+	{ 1713, 2,  4 },
+	{ 29667, 3,  5 },
+	{ 29646, 4,  8 },
+	{ 29672, 5,  8 },
+};
+#else
+static const Il2CppMethodExecutionContextInfo g_methodExecutionContextInfos[1] = { { 0, 0, 0 } };
+#endif
+#if IL2CPP_MONO_DEBUGGER
+static const char* g_methodExecutionContextInfoStrings[6] = 
+{
+	"low",
+	"high",
+	"baseSubsystem",
+	"loader",
+	"settingsInstance",
+	"managerSettings",
+};
+#else
+static const char* g_methodExecutionContextInfoStrings[1] = { NULL };
+#endif
+#if IL2CPP_MONO_DEBUGGER
+static const Il2CppMethodExecutionContextInfoIndex g_methodExecutionContextInfoIndexes[11] = 
+{
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 0, 2 },
+	{ 0, 0 },
+	{ 2, 1 },
+	{ 3, 1 },
+	{ 0, 0 },
+	{ 0, 0 },
+	{ 4, 2 },
+};
+#else
+static const Il2CppMethodExecutionContextInfoIndex g_methodExecutionContextInfoIndexes[1] = { { 0, 0} };
+#endif
+#if IL2CPP_MONO_DEBUGGER
+IL2CPP_EXTERN_C Il2CppSequencePoint g_sequencePointsUnity_XR_ARFoundation_InternalUtils[];
+Il2CppSequencePoint g_sequencePointsUnity_XR_ARFoundation_InternalUtils[94] = 
+{
+	{ 84655, 0, 0, 0, 0, 0, -1, kSequencePointKind_Normal, 0, 0 },
+	{ 84655, 0, 0, 0, 0, 0, 16777215, kSequencePointKind_Normal, 0, 1 },
+	{ 84655, 1, 56, 56, 9, 10, 0, kSequencePointKind_Normal, 0, 2 },
+	{ 84655, 1, 57, 63, 13, 15, 1, kSequencePointKind_Normal, 0, 3 },
+	{ 84655, 1, 57, 63, 13, 15, 27, kSequencePointKind_StepOut, 0, 4 },
+	{ 84655, 1, 57, 63, 13, 15, 55, kSequencePointKind_StepOut, 0, 5 },
+	{ 84655, 1, 64, 64, 9, 10, 93, kSequencePointKind_Normal, 0, 6 },
+	{ 84657, 0, 0, 0, 0, 0, -1, kSequencePointKind_Normal, 0, 7 },
+	{ 84657, 0, 0, 0, 0, 0, 16777215, kSequencePointKind_Normal, 0, 8 },
+	{ 84657, 2, 13, 13, 9, 10, 0, kSequencePointKind_Normal, 0, 9 },
+	{ 84657, 2, 14, 14, 13, 99, 1, kSequencePointKind_Normal, 0, 10 },
+	{ 84657, 2, 14, 14, 13, 99, 13, kSequencePointKind_StepOut, 0, 11 },
+	{ 84657, 2, 14, 14, 13, 99, 30, kSequencePointKind_StepOut, 0, 12 },
+	{ 84657, 2, 14, 14, 13, 99, 35, kSequencePointKind_StepOut, 0, 13 },
+	{ 84657, 2, 14, 14, 13, 99, 40, kSequencePointKind_StepOut, 0, 14 },
+	{ 84657, 2, 15, 15, 9, 10, 48, kSequencePointKind_Normal, 0, 15 },
+	{ 84658, 0, 0, 0, 0, 0, -1, kSequencePointKind_Normal, 0, 16 },
+	{ 84658, 0, 0, 0, 0, 0, 16777215, kSequencePointKind_Normal, 0, 17 },
+	{ 84658, 2, 25, 25, 9, 10, 0, kSequencePointKind_Normal, 0, 18 },
+	{ 84658, 2, 26, 26, 13, 95, 1, kSequencePointKind_Normal, 0, 19 },
+	{ 84658, 2, 26, 26, 13, 95, 13, kSequencePointKind_StepOut, 0, 20 },
+	{ 84658, 2, 26, 26, 13, 95, 30, kSequencePointKind_StepOut, 0, 21 },
+	{ 84658, 2, 26, 26, 13, 95, 35, kSequencePointKind_StepOut, 0, 22 },
+	{ 84658, 2, 27, 27, 9, 10, 43, kSequencePointKind_Normal, 0, 23 },
+	{ 84659, 0, 0, 0, 0, 0, -1, kSequencePointKind_Normal, 0, 24 },
+	{ 84659, 0, 0, 0, 0, 0, 16777215, kSequencePointKind_Normal, 0, 25 },
+	{ 84659, 3, 14, 14, 9, 10, 0, kSequencePointKind_Normal, 0, 26 },
+	{ 84659, 3, 15, 15, 13, 55, 1, kSequencePointKind_Normal, 0, 27 },
+	{ 84659, 3, 15, 15, 13, 55, 6, kSequencePointKind_StepOut, 0, 28 },
+	{ 84659, 3, 16, 16, 13, 65, 12, kSequencePointKind_Normal, 0, 29 },
+	{ 84659, 3, 16, 16, 13, 65, 14, kSequencePointKind_StepOut, 0, 30 },
+	{ 84659, 3, 17, 17, 9, 10, 22, kSequencePointKind_Normal, 0, 31 },
+	{ 84661, 0, 0, 0, 0, 0, -1, kSequencePointKind_Normal, 0, 32 },
+	{ 84661, 0, 0, 0, 0, 0, 16777215, kSequencePointKind_Normal, 0, 33 },
+	{ 84661, 4, 23, 23, 9, 10, 0, kSequencePointKind_Normal, 0, 34 },
+	{ 84661, 4, 24, 24, 13, 74, 1, kSequencePointKind_Normal, 0, 35 },
+	{ 84661, 4, 24, 24, 13, 74, 3, kSequencePointKind_StepOut, 0, 36 },
+	{ 84661, 4, 25, 25, 13, 53, 9, kSequencePointKind_Normal, 0, 37 },
+	{ 84661, 4, 26, 26, 13, 38, 31, kSequencePointKind_Normal, 0, 38 },
+	{ 84661, 4, 27, 27, 9, 10, 48, kSequencePointKind_Normal, 0, 39 },
+	{ 84662, 0, 0, 0, 0, 0, -1, kSequencePointKind_Normal, 0, 40 },
+	{ 84662, 0, 0, 0, 0, 0, 16777215, kSequencePointKind_Normal, 0, 41 },
+	{ 84662, 4, 38, 38, 9, 10, 0, kSequencePointKind_Normal, 0, 42 },
+	{ 84662, 4, 39, 39, 13, 98, 1, kSequencePointKind_Normal, 0, 43 },
+	{ 84662, 4, 39, 39, 13, 98, 1, kSequencePointKind_StepOut, 0, 44 },
+	{ 84662, 4, 39, 39, 13, 98, 7, kSequencePointKind_StepOut, 0, 45 },
+	{ 84662, 4, 39, 39, 13, 98, 14, kSequencePointKind_StepOut, 0, 46 },
+	{ 84662, 4, 39, 39, 13, 98, 19, kSequencePointKind_StepOut, 0, 47 },
+	{ 84662, 4, 39, 39, 13, 98, 25, kSequencePointKind_StepOut, 0, 48 },
+	{ 84662, 4, 39, 39, 0, 0, 34, kSequencePointKind_Normal, 0, 49 },
+	{ 84662, 4, 40, 40, 13, 14, 37, kSequencePointKind_Normal, 0, 50 },
+	{ 84662, 4, 41, 41, 17, 34, 38, kSequencePointKind_Normal, 0, 51 },
+	{ 84662, 4, 42, 42, 17, 30, 45, kSequencePointKind_Normal, 0, 52 },
+	{ 84662, 4, 45, 45, 13, 74, 49, kSequencePointKind_Normal, 0, 53 },
+	{ 84662, 4, 45, 45, 13, 74, 49, kSequencePointKind_StepOut, 0, 54 },
+	{ 84662, 4, 45, 45, 13, 74, 54, kSequencePointKind_StepOut, 0, 55 },
+	{ 84662, 4, 45, 45, 13, 74, 59, kSequencePointKind_StepOut, 0, 56 },
+	{ 84662, 4, 48, 48, 13, 93, 65, kSequencePointKind_Normal, 0, 57 },
+	{ 84662, 4, 48, 48, 13, 93, 68, kSequencePointKind_StepOut, 0, 58 },
+	{ 84662, 4, 48, 48, 13, 93, 87, kSequencePointKind_StepOut, 0, 59 },
+	{ 84662, 4, 50, 50, 13, 38, 97, kSequencePointKind_Normal, 0, 60 },
+	{ 84662, 4, 51, 51, 9, 10, 114, kSequencePointKind_Normal, 0, 61 },
+	{ 84663, 0, 0, 0, 0, 0, -1, kSequencePointKind_Normal, 0, 62 },
+	{ 84663, 0, 0, 0, 0, 0, 16777215, kSequencePointKind_Normal, 0, 63 },
+	{ 84663, 5, 8, 8, 9, 10, 0, kSequencePointKind_Normal, 0, 64 },
+	{ 84663, 5, 9, 9, 13, 83, 1, kSequencePointKind_Normal, 0, 65 },
+	{ 84663, 5, 9, 9, 13, 83, 18, kSequencePointKind_StepOut, 0, 66 },
+	{ 84663, 5, 10, 10, 9, 10, 26, kSequencePointKind_Normal, 0, 67 },
+	{ 84664, 0, 0, 0, 0, 0, -1, kSequencePointKind_Normal, 0, 68 },
+	{ 84664, 0, 0, 0, 0, 0, 16777215, kSequencePointKind_Normal, 0, 69 },
+	{ 84664, 5, 13, 13, 9, 10, 0, kSequencePointKind_Normal, 0, 70 },
+	{ 84664, 5, 14, 14, 13, 66, 1, kSequencePointKind_Normal, 0, 71 },
+	{ 84664, 5, 14, 14, 13, 66, 18, kSequencePointKind_StepOut, 0, 72 },
+	{ 84664, 5, 15, 15, 9, 10, 26, kSequencePointKind_Normal, 0, 73 },
+	{ 84665, 0, 0, 0, 0, 0, -1, kSequencePointKind_Normal, 0, 74 },
+	{ 84665, 0, 0, 0, 0, 0, 16777215, kSequencePointKind_Normal, 0, 75 },
+	{ 84665, 6, 16, 16, 9, 10, 0, kSequencePointKind_Normal, 0, 76 },
+	{ 84665, 6, 17, 17, 13, 63, 1, kSequencePointKind_Normal, 0, 77 },
+	{ 84665, 6, 17, 17, 13, 63, 1, kSequencePointKind_StepOut, 0, 78 },
+	{ 84665, 6, 18, 18, 13, 42, 7, kSequencePointKind_Normal, 0, 79 },
+	{ 84665, 6, 18, 18, 13, 42, 9, kSequencePointKind_StepOut, 0, 80 },
+	{ 84665, 6, 18, 18, 0, 0, 15, kSequencePointKind_Normal, 0, 81 },
+	{ 84665, 6, 19, 19, 17, 30, 18, kSequencePointKind_Normal, 0, 82 },
+	{ 84665, 6, 21, 21, 13, 60, 22, kSequencePointKind_Normal, 0, 83 },
+	{ 84665, 6, 21, 21, 13, 60, 23, kSequencePointKind_StepOut, 0, 84 },
+	{ 84665, 6, 22, 22, 13, 41, 29, kSequencePointKind_Normal, 0, 85 },
+	{ 84665, 6, 22, 22, 13, 41, 31, kSequencePointKind_StepOut, 0, 86 },
+	{ 84665, 6, 22, 22, 0, 0, 38, kSequencePointKind_Normal, 0, 87 },
+	{ 84665, 6, 23, 23, 17, 30, 42, kSequencePointKind_Normal, 0, 88 },
+	{ 84665, 6, 25, 25, 13, 84, 46, kSequencePointKind_Normal, 0, 89 },
+	{ 84665, 6, 25, 25, 13, 84, 47, kSequencePointKind_StepOut, 0, 90 },
+	{ 84665, 6, 25, 25, 13, 84, 59, kSequencePointKind_StepOut, 0, 91 },
+	{ 84665, 6, 25, 25, 13, 84, 64, kSequencePointKind_StepOut, 0, 92 },
+	{ 84665, 6, 26, 26, 9, 10, 72, kSequencePointKind_Normal, 0, 93 },
+};
+#else
+extern Il2CppSequencePoint g_sequencePointsUnity_XR_ARFoundation_InternalUtils[];
+Il2CppSequencePoint g_sequencePointsUnity_XR_ARFoundation_InternalUtils[1] = { { 0, 0, 0, 0, 0, 0, 0, kSequencePointKind_Normal, 0, 0, } };
+#endif
+#if IL2CPP_MONO_DEBUGGER
+static const Il2CppCatchPoint g_catchPoints[1] = { { 0, 0, 0, 0, } };
+#else
+static const Il2CppCatchPoint g_catchPoints[1] = { { 0, 0, 0, 0, } };
+#endif
+#if IL2CPP_MONO_DEBUGGER
+static const Il2CppSequencePointSourceFile g_sequencePointSourceFiles[] = {
+{ "", { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0} },
+{ ".\\Unity.SourceGenerators\\Unity.MonoScriptGenerator.MonoScriptInfoGenerator\\AssemblyMonoScriptTypes.generated.cs", { 156, 147, 172, 68, 198, 215, 195, 155, 69, 47, 239, 147, 249, 166, 41, 136} },
+{ ".\\Library\\PackageCache\\com.unity.xr.arfoundation@632379b1f7b5\\Runtime\\InternalUtils\\PoseUtils.cs", { 217, 99, 4, 251, 246, 184, 84, 162, 188, 133, 59, 54, 84, 182, 129, 115} },
+{ ".\\Library\\PackageCache\\com.unity.xr.arfoundation@632379b1f7b5\\Runtime\\InternalUtils\\SerializableGuidUtility.cs", { 39, 125, 156, 195, 120, 236, 228, 172, 238, 49, 244, 177, 239, 171, 58, 71} },
+{ ".\\Library\\PackageCache\\com.unity.xr.arfoundation@632379b1f7b5\\Runtime\\InternalUtils\\SubsystemUtils.cs", { 175, 88, 194, 250, 170, 186, 209, 59, 167, 163, 221, 136, 163, 225, 136, 234} },
+{ ".\\Library\\PackageCache\\com.unity.xr.arfoundation@632379b1f7b5\\Runtime\\InternalUtils\\UnitConversionUtility.cs", { 174, 127, 176, 121, 241, 76, 53, 241, 6, 46, 93, 92, 30, 128, 110, 71} },
+{ ".\\Library\\PackageCache\\com.unity.xr.arfoundation@632379b1f7b5\\Runtime\\InternalUtils\\XRManagerUtility.cs", { 242, 201, 5, 241, 81, 9, 0, 134, 160, 38, 126, 6, 156, 101, 145, 176} },
+};
+#else
+static const Il2CppSequencePointSourceFile g_sequencePointSourceFiles[1] = { NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+#endif
+#if IL2CPP_MONO_DEBUGGER
+static const Il2CppTypeSourceFilePair g_typeSourceFiles[6] = 
+{
+	{ 10554, 1 },
+	{ 10555, 2 },
+	{ 10556, 3 },
+	{ 10558, 4 },
+	{ 10559, 5 },
+	{ 10560, 6 },
+};
+#else
+static const Il2CppTypeSourceFilePair g_typeSourceFiles[1] = { { 0, 0 } };
+#endif
+#if IL2CPP_MONO_DEBUGGER
+static const Il2CppMethodScope g_methodScopes[9] = 
+{
+	{ 0, 95 },
+	{ 0, 50 },
+	{ 0, 45 },
+	{ 0, 24 },
+	{ 0, 50 },
+	{ 0, 116 },
+	{ 0, 28 },
+	{ 0, 28 },
+	{ 0, 74 },
+};
+#else
+static const Il2CppMethodScope g_methodScopes[1] = { { 0, 0 } };
+#endif
+#if IL2CPP_MONO_DEBUGGER
+static const Il2CppMethodHeaderInfo g_methodHeaderInfos[11] = 
+{
+	{ 95, 0, 1 },
+	{ 0, 0, 0 },
+	{ 50, 1, 1 },
+	{ 45, 2, 1 },
+	{ 24, 3, 1 },
+	{ 0, 0, 0 },
+	{ 50, 4, 1 },
+	{ 116, 5, 1 },
+	{ 28, 6, 1 },
+	{ 28, 7, 1 },
+	{ 74, 8, 1 },
+};
+#else
+static const Il2CppMethodHeaderInfo g_methodHeaderInfos[1] = { { 0, 0, 0 } };
+#endif
+IL2CPP_EXTERN_C const Il2CppDebuggerMetadataRegistration g_DebuggerMetadataRegistrationUnity_XR_ARFoundation_InternalUtils;
+const Il2CppDebuggerMetadataRegistration g_DebuggerMetadataRegistrationUnity_XR_ARFoundation_InternalUtils = 
+{
+	(Il2CppMethodExecutionContextInfo*)g_methodExecutionContextInfos,
+	(Il2CppMethodExecutionContextInfoIndex*)g_methodExecutionContextInfoIndexes,
+	(Il2CppMethodScope*)g_methodScopes,
+	(Il2CppMethodHeaderInfo*)g_methodHeaderInfos,
+	(Il2CppSequencePointSourceFile*)g_sequencePointSourceFiles,
+	94,
+	(Il2CppSequencePoint*)g_sequencePointsUnity_XR_ARFoundation_InternalUtils,
+	0,
+	(Il2CppCatchPoint*)g_catchPoints,
+	6,
+	(Il2CppTypeSourceFilePair*)g_typeSourceFiles,
+	(const char**)g_methodExecutionContextInfoStrings,
+};
